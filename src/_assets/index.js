0062c5238591b44a653a291fbc66e21356e5b2f6
Muhamad Fajri <hurifajri@gmail.com>
2019-10-23 14:10:50 +0700

    Add assets and style for dashboard

--- src/_assets/index.js
+++ src/_assets/index.js
@@ -1,4 +1,9 @@
-// @ts-ignore
+// @ts-nocheck
 import logo from './le-logo.png'
+import burger from './burger.png'
+import mail from './mail.png'
+import line from './line.png'
+import oval from './oval.png'
+import arrow from './arrow.png'
 
-export { logo }
+export { logo, burger, mail, line, oval, arrow }
